789d49df2ff85b283a45557704aab18ad06d12c6
Lewis Jones <lewis.jones1997@protonmail.com>
2021-10-19 09:41:41 +0100

    feat(database_connection): add database connection class

---
+++ lib/database_connection_setup.rb
@@ -0,0 +1,9 @@
+require './lib/database_connection'
+
+if ENV['ENVIRONMENT'] == 'test'
+  DatabaseConnection.setup('makersbnb_test')
+else
+  # :nocov:
+  DatabaseConnection.setup('makersbnb')
+  # :nocov:
+end
