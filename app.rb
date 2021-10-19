d6ae554ccc46ebf2e849c540be63a8364fe78b6e
Alex Wintle <alexwintle663@gmail.com>
2021-10-19 16:00:42 +0100

    cleanup: app.rb removed whitespace rubocop complaints

--- app.rb
+++ app.rb
@@ -9,33 +9,33 @@ class MakersBnb < Sinatra::Base
   end
 
   get '/sign-up' do
-    erb (:sign_up)
+    erb(:sign_up)
   end
 
   post '/sign-up' do
     p params
     User.create(params['username'], params['email'], params['password'])
-    redirect ('/sign-up-completed')
+    redirect('/sign-up-completed')
   end
 
   get '/sign-up-completed' do
-    erb (:sign_up_completed)
+    erb(:sign_up_completed)
   end
 
   get '/' do
-    erb :index
+    erb(:index)
   end
 
   get '/add-space' do
-    erb :add_space
+    erb(:add_space)
   end
 
   get '/add-space-confirmation' do
-    erb :add_space_confirmation
+    erb(:add_space_confirmation)
   end
 
   post '/add-space' do
     Space.add(space_name: params[:space_name], description: params[:description], rate: params[:rate])
-    redirect '/add-space-confirmation'
+    redirect('/add-space-confirmation')
   end
 end
