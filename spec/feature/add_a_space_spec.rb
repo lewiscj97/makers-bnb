5bb8a1e2ad08cd90efbf2945063a3307b8dd831e
Alex Wintle <alexwintle663@gmail.com>
2021-10-19 12:51:29 +0100

    cleanup: made add_a_space_spec.rb test work for current redirect

--- spec/feature/add_a_space_spec.rb
+++ spec/feature/add_a_space_spec.rb
@@ -1,6 +1,6 @@
 feature 'add a space: ' do
   scenario 'it should add a space to the database' do
     visit('/add-space')
-    expect(page).to have_content('Space successfully added!')
+    expect(page).to have_content('Makers BnB\nAdd a Space\nSpace Name: Description: Nightly Rate:')
   end
 end
