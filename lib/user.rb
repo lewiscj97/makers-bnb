f833ad3e70d8af7be269979d8140dc889cde85ff
michelle <michelle.watkins@engineering.digital.dwp.gov.uk>
2021-10-20 12:04:22 +0100

    Fixed further offenses

--- lib/user.rb
+++ lib/user.rb
@@ -20,7 +20,10 @@ class User
   end
 
   def self.create(username, email, password)
-    result = DatabaseConnection.query('INSERT INTO users (username, email, password) VALUES($1, $2, $3) RETURNING id, username, email, password;', [username, email, password])
+    result = DatabaseConnection.query(
+      'INSERT INTO users (username, email, password) VALUES($1, $2, $3) RETURNING id, username, email, password;', [username,
+                                                                                                                    email, password]
+    )
     User.new(result[0]['id'], result[0]['username'], result[0]['email'], result[0]['password'])
   end
 end
