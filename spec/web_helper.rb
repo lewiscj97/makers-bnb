a02ee29156e6e81c930c31ea3cf7941d2ac9222c
michelle <michelle.watkins@engineering.digital.dwp.gov.uk>
2021-10-20 11:36:49 +0100

    Fixed web_helper offences

--- spec/web_helper.rb
+++ spec/web_helper.rb
@@ -1,9 +1,11 @@
+# frozen_string_literal: true
+
 def drop_test_db
-  p "Setting up test database..."
+  p 'Setting up test database...'
   connection = PG.connect(dbname: 'makersbnb_test')
   # Clear the tables
-  connection.exec("TRUNCATE users;")
-  connection.exec("TRUNCATE spaces;")
+  connection.exec('TRUNCATE users;')
+  connection.exec('TRUNCATE spaces;')
 end
 
 def user_sign_up
