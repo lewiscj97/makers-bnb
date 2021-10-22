98c5de3afca4451cba35b35782f3b2369ad85ba0
Lewis Jones <lewis.jones1997@protonmail.com>
2021-10-22 10:34:47 +0100

    chore(*): tidy up

--- lib/booking.rb
+++ lib/booking.rb
@@ -16,12 +16,4 @@ class Booking
       VALUES($1, $2, $3, $4, $5);', [space_id, user_id, date_from, date_to, 0]
     )
   end
-
-  def self.find_by_id(user_id)
-    result = DatabaseConnection.query('SELECT * FROM bookings WHERE user_id=$1;', [user_id])
-    result.map do |value|
-      Booking.new(id: value['id'], space_id: value['space_id'], user_id: value['user_id'],
-      date_from: value['date_from'], date_to: value['date_to'], confirmed: value['confirmed'])
-    end
-  end
 end
--- spec/lib/booking_spec.rb
+++ spec/lib/booking_spec.rb
@@ -33,26 +33,4 @@ describe Booking do
       expect(result['confirmed']).to eq '0'
     end
   end
-
-  describe '#find_by_id' do
-    it 'will return a booking' do
-      owner = DatabaseConnection.query("INSERT INTO users(id, username, email, password) VALUES(1, 'Foo', 'foo@bar.com', 'password');")
-      customer = DatabaseConnection.query("INSERT INTO users(username, email, password) VALUES('Bar', 'bar@bar.com', 'password');")
-      
-      # List a space with the user_id set to the owner id
-      DatabaseConnection.query("INSERT INTO spaces(id, space_name, description, rate, user_id) VALUES(1, 'Space', 'A lovely home', 50 , 1);")
-      Booking.create(space_id: 1, user_id: 1, date_from: '2021-10-20', date_to: '2021-10-23')
-      
-      Booking.find_by_id(user_id: 1)
-      
-      response = DatabaseConnection.query('SELECT * FROM bookings WHERE user_id=1;')
-      result = response.first
-
-      expect(result['date_from']).to eq '2021-10-20'
-      expect(result['date_to']).to eq '2021-10-23'
-      expect(result['space_id']).to eq '1'
-      expect(result['user_id']).to eq '1'
-      expect(result['confirmed']).to eq '0'
-    end
-  end
 end
