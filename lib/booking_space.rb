ee94df884727819508fcfeb8f8b994c13a619324
Stella Martin <stellamartin222@gmail.com>
2021-10-22 09:30:42 +0100

    fixed username issues

--- lib/booking_space.rb
+++ lib/booking_space.rb
@@ -1,7 +1,7 @@
 class BookingSpace
   attr_reader :id, :space_id, :user_id, :date_from, :date_to, :confirmed
   
-  def initialize(id:, space_id:, user_id:, date_from:, date_to:, confirmed: 0)
+  def initialize(id:, date_from:, date_to:, confirmed: 0, space_id:, description:, rate:, customer_id:)
     @id = id
     @date_from = date_from
     @date_to = date_to
@@ -9,13 +9,19 @@ class BookingSpace
     @space_name = space_name
     @description = description
     @rate = rate
+    @customer_id = customer_id
   end
 
-  def self.get_by_id(user_id)
-    results = DatabaseConnection.query('SELECT bookings.id, date_from, date_to, confirmed, space_name, description, rate FROM bookings JOIN spaces ON bookings.space_id = spaces.id WHERE spaces.user_id = $1;', [user_id])
+  def self.get_by_id(customer_id)
+    results = DatabaseConnection.query('
+    SELECT bookings.id, date_from, date_to, confirmed, space_name, description, rate, bookings.user_id FROM users
+    JOIN spaces ON users.id=spaces.user_id
+    JOIN bookings ON spaces.user_id = users.id
+    WHERE users.id = $1
+    ;', [customer_id])
     result.map do |value|
       BookingSpace.new(id: value['id'], date_from: value['date_from'], date_to: value['date_to'], confirmed: value['confirmed'],
-                    space_name: value['space_name'], description: value['description'], rate: value['rate'])
+                    space_name: value['space_name'], description: value['description'], rate: value['rate'], customer_id: value['customer_id'])
     end
   end
 
--- spec/lib/booking_space_spec.rb
+++ spec/lib/booking_space_spec.rb
@@ -4,9 +4,16 @@ describe '#booingSpace' do
       owner = DatabaseConnection.query("INSERT INTO users(id, username, email, password) VALUES(1, 'Foo', 'foo@bar.com', 'password');")
       customer = DatabaseConnection.query("INSERT INTO users(username, email, password) VALUES('Bar', 'bar@bar.com', 'password');")
       DatabaseConnection.query("INSERT INTO spaces(id, space_name, description, rate, user_id) VALUES(1, 'Space', 'A lovely home', 50 , 1);")
-      DatabaseConnection.query("INSERT INTO bookings(space_id, user_id, date_from, date_to, confirmed) VALUES(1, 1, '2021-10-20', '2021-10-25', 0"))
+      DatabaseConnection.query("INSERT INTO bookings(space_id, user_id, date_from, date_to, confirmed) VALUES(1, 1, '2021-10-20', '2021-10-25', '0');")
 
       response = DatabaseConnection.query("'SELECT bookings.id, date_from, date_to, confirmed, space_name, description, rate FROM bookings JOIN spaces ON bookings.space_id = spaces.id WHERE spaces.user_id = 1;'")
+      result = response.first
+
+      expect(result['date_from']).to eq '2021-10-20'
+      expect(result['date_to']).to eq '2021-10-25'
+      expect(result['space_id']).to eq '1'
+      expect(result['user_id']).to eq '1'
+      expect(result['confirmed']).to eq '0'
     end
   end
 end
