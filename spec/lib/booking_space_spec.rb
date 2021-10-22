eeac19232a949e4e8d74ecb1bc147d33b7bdf79f
Stella Martin <stellamartin222@gmail.com>
2021-10-22 09:35:59 +0100

    Update booking_space_spec.rb

--- spec/lib/booking_space_spec.rb
+++ spec/lib/booking_space_spec.rb
@@ -6,14 +6,21 @@ describe '#booingSpace' do
       DatabaseConnection.query("INSERT INTO spaces(id, space_name, description, rate, user_id) VALUES(1, 'Space', 'A lovely home', 50 , 1);")
       DatabaseConnection.query("INSERT INTO bookings(space_id, user_id, date_from, date_to, confirmed) VALUES(1, 1, '2021-10-20', '2021-10-25', '0');")
 
-      response = DatabaseConnection.query("'SELECT bookings.id, date_from, date_to, confirmed, space_name, description, rate FROM bookings JOIN spaces ON bookings.space_id = spaces.id WHERE spaces.user_id = 1;'")
+      response = DatabaseConnection.query(
+        'SELECT bookings.id, date_from, date_to, confirmed, space_name, description, rate, bookings.user_id FROM users
+        JOIN spaces ON users.id=spaces.user_id
+        JOIN bookings ON spaces.user_id = users.id
+        WHERE users.id = 1'
+      )
       result = response.first
 
       expect(result['date_from']).to eq '2021-10-20'
       expect(result['date_to']).to eq '2021-10-25'
-      expect(result['space_id']).to eq '1'
-      expect(result['user_id']).to eq '1'
       expect(result['confirmed']).to eq '0'
+      expect(result['space_name']).to eq 'Space'
+      expect(result['description']).to eq 'A lovely home'
+      expect(result['rate']).to eq '50'
+      expect(result['user_id']).to eq '1'
     end
   end
 end
